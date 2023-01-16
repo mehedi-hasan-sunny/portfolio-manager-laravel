609f5a58eaba84e9c69a8db9e5105d3be64ccab5
Mehedi <hasansunny.mehedi@gmail.com>
2023-01-16 10:30:19 +0600

    Project setup and migration files created.

---
+++ database/migrations/2023_01_15_094403_create_education_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('education', function (Blueprint $table) {
+            $table->id();
+            $table->text('title');
+            $table->string('institution');
+            $table->longText('description');
+            $table->string('major_in');
+            $table->year('passing_year')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('education');
+    }
+};
---
+++ database/migrations/2023_01_15_094455_create_blogs_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('blogs', function (Blueprint $table) {
+            $table->id();
+            $table->text('title');
+            $table->text('sub_title');
+            $table->string('cover_photo');
+            $table->longText('content');
+            $table->longText('summary');
+            $table->text('text');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('blogs');
+    }
+};
